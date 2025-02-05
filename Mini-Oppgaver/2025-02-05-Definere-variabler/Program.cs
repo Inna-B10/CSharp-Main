186f4944cdce8f8c405cb99132133c930dfad846
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-05 21:04:48 +0100

    get numbers from user, check, convert and sum them

--- Mini-Oppgaver/2025-02-05-Definere-variabler/Program.cs
+++ Mini-Oppgaver/2025-02-05-Definere-variabler/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace _2025_02_05_Definere_variabler;
+﻿using System;
+using System.Globalization;
+
+namespace _2025_02_05_Definere_variabler;
 
 class Program
 {
@@ -12,6 +15,7 @@ class Program
         Console.WriteLine("My cat's name is Kåre. He has the following age: {0}", catAge);
         Console.WriteLine("-----------------------------------------------------");
 
+        /* ----------------------------- Sum Two Numbers ---------------------------- */
         Console.WriteLine($"I have the following numbers:  {num1} and {num2}");
         Console.WriteLine("I want to sum these numbers as:");
         Console.WriteLine();
@@ -31,11 +35,41 @@ class Program
         Sum(4);
         Console.WriteLine();
 
+        /* -------------------------- Get Numbers From User ------------------------- */
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("enter your first number");
+        string userNum1 = Console.ReadLine();
+
+        object result1 = ConvertNumber(userNum1);
+
+        Console.WriteLine("enter your second number");
+        string userNum2 = Console.ReadLine();
+
+        object result2 = ConvertNumber(userNum2);
+
+        Console.WriteLine($"Type: {result1.GetType()}, Value: {result1}");
+        Console.WriteLine($"Type: {result2.GetType()}, Value: {result2}");
+
+        Console.WriteLine();
+        if (result1.GetType() != typeof(string) && result2.GetType() != typeof(string))
+        {
+            // func. ConvertNumber() return object, first convert!
+            // decimal sum = Convert.ToDecimal(result1) + Convert.ToDecimal(result2);
+
+            decimal sum = (decimal)result1 + (decimal)result2;
+            Console.WriteLine($"Sum of {result1} and {result2} is: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Cannot operate with non-number types!");
+        }
+
+
+
         static void Sum(int type = 0)
         {
             switch (type)
             {
-
                 case 1:
                     Console.WriteLine("float, float");
                     Console.Write($"sum of {(float)num1} and {(double)num2}");
@@ -70,6 +104,34 @@ class Program
 
             }
         }
-        // string userNum = Console.ReadLine();
+
+        static object ConvertNumber(string userNum)
+        {
+            // Convert comma to dot if exists (42,5 -> 42.5)
+            if (userNum.Contains(','))
+                userNum = userNum.Replace(',', '.');
+
+            //NB NumberStyles.Any, CultureInfo.InvariantCulture,
+            /* Pass CultureInfo.InvariantCulture to TryParse.
+            By default, TryParse uses the OS regional settings, but we can explicitly specify the English format(where the separator is a period .) for numbers like 42.5 or 42,5 */
+
+            //first check if it is number
+            if (decimal.TryParse(userNum, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal decimalNumber))
+            {
+                /*//check if it is possible to convert decimal to something else
+                if (int.TryParse(userNum, NumberStyles.Any, CultureInfo.InvariantCulture, out int intNumber))
+                    return intNumber;
+                if (long.TryParse(userNum, NumberStyles.Any, CultureInfo.InvariantCulture, out long longNumber))
+                    return longNumber;
+                if (float.TryParse(userNum, NumberStyles.Any, CultureInfo.InvariantCulture, out float floatNumber))
+                    return floatNumber;
+                if (double.TryParse(userNum, NumberStyles.Any, CultureInfo.InvariantCulture, out double doubleNumber))
+                    return doubleNumber;*/
+
+                return decimalNumber; //if not int/long/float/double
+            }
+
+            return "not a number";
+        }
     }
 }
