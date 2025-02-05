d2a5a8b4776e8b54236d5140b4e85ff35cb60f4d
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-05 19:04:42 +0100

    new folders with study materials added

---
+++ Study-Materials/2025-02-03-CSharp-intro-operasjoner/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _03_02_2025_intro_operasjoner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1 = 11;
+        double num2 = 32;
+        
+
+        /* Aritremiske operatorer */
+        double sum = num1  +num2;
+        Console.WriteLine(sum);
+
+        double subtract = num1 - num2;
+        Console.WriteLine(subtract);
+
+        double multiply = num1 * num2;
+        Console.WriteLine(multiply);
+
+        double divide = num1 / num2;
+        Console.WriteLine(divide);
+
+
+
+        /* Tildelingsoperatorer. */
+        num1 += 2;
+
+        num2 -= 4;
+
+        num1 *= 3;
+
+        num2 /= 5;
+
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+
+
+        /* Sammenlignings operatorer, resultatet kan lagres som en bool (true/false) */
+        bool isLargerThan = num1 > num2;
+        Console.WriteLine(isLargerThan);
+
+        bool isSmallerThan = num1 < num2;
+        Console.WriteLine(isSmallerThan);
+
+        bool isEqual = num1 == num2;
+        Console.WriteLine(isEqual);
+
+        /* Logikkoperatorer, lar oss se på flere forskjellige sammenligninger */
+
+        /* Er begge sann? */
+        Console.WriteLine(isLargerThan && isSmallerThan);
+
+        /* er en av de sann? */
+        Console.WriteLine(isLargerThan || isSmallerThan);
+
+        /* det motsatte av */
+        Console.WriteLine(!isEqual);
+    }
+}
---
+++ Study-Materials/2025-02-03-CSharp-intro-runtime/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03_02_2025_intro_runtime;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        /* Her definerer vi en variabel som heter helloWorld.
+        Vi sier den skal ha datatypen string, og gir den verdien "Hello, World!"
+        
+        Når vi definerer en variabel i C# må vi gi den:
+            1. datatypen
+            2. et variabelnavn
+            3. en verdi den skal holde.  */
+        string helloWorld = "Hello, World!";
+        Console.WriteLine(helloWorld);
+        Console.WriteLine("Second Line in Program.Main");
+
+        //for å definere et helt tall, bruk datatypen int.
+        int myAge = 33;
+
+        //for å definere et desimaltall, bruk datatypen double.
+        double myHeight = 1.87;
+        Console.WriteLine(myAge);
+        Console.WriteLine(myHeight);
+
+        //Andre måter å definere tall på. 
+        float myFloat = 1.43f;  
+        decimal myDecimal = 4.85m;
+        Console.WriteLine(myFloat);
+        Console.WriteLine(myDecimal);
+
+
+        //Alle primærdatatypene våre har også en maksverdi. 
+        int tooLargeNumber = int.MaxValue;
+        Console.WriteLine(tooLargeNumber); 
+
+        //Det samme gjelder desimaltall, legger vi til et desimaltall til på begge disse, klarer ikke datamaskinen lengre å se forskjell på dette tallet og tallet 1. 
+        float tooManyDesimalPoints = 0.9999999f;
+        double tooManyDesimalsDouble = 0.9999999999999999;
+        Console.WriteLine(tooManyDesimalPoints);
+        Console.WriteLine(tooManyDesimalsDouble);
+    }
+}
