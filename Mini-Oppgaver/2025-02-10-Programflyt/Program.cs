2e0bfcfb05dbc87fdc7b39cae52edc3cab8ddfc5
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-11 03:54:52 +0100

    check winner and show result

--- Mini-Oppgaver/2025-02-10-Programflyt/Program.cs
+++ Mini-Oppgaver/2025-02-10-Programflyt/Program.cs
@@ -5,26 +5,73 @@ class Program
     static void Main(string[] args)
     {
         string[] options = { "rock", "paper", "scissors" };
-        // Console.WriteLine(options[0]);
-
-
-
-
 
         string? start = startGame();
-        if (start == "n") { return; }
-        Console.WriteLine("-----------------Game started-----------------");
 
-        string randomPcChoice = pcChoice(options);
-        Console.WriteLine("PC choice is: ***********");
-        Console.WriteLine("What is your choice?");
-        string? userChoice = getUserChoice();
+        while (start == "y")
+        {
+            Console.WriteLine("-----------------Game started-----------------");
 
-        Console.WriteLine();
-        Console.WriteLine($"User's choice is: {userChoice}");
-        Console.WriteLine($"PC's choice is: {randomPcChoice}");
+            string randomPcChoice = pcChoice(options);
+            Console.WriteLine("PC choice is: *********************");
+            Console.WriteLine("What is your choice?");
+            string? userChoice = getUserChoice();
 
+            Console.WriteLine();
+            Console.WriteLine("--------------------Result-------------------");
+            Console.WriteLine($"User's choice is: {userChoice}");
+            Console.WriteLine($"PC's choice is: {randomPcChoice}");
+            string winner = checkWinner(userChoice, randomPcChoice);
 
+            Console.WriteLine();
+            Console.WriteLine(winner);
+            if (winner == "The user wins!")
+            {
+                Console.WriteLine("Congratulation!");
+            }
+            Console.WriteLine("----------------Game finished----------------");
+            startGame();
+        }
+        return;
+
+        string checkWinner(string userChoice, string randomPcChoice)
+        {
+            if (userChoice == randomPcChoice)
+            {
+                return "Draw!";
+            }
+            switch (userChoice)
+            {
+                case "rock":
+                    if (randomPcChoice == "scissors")
+                    {
+                        return "The user wins!";
+                    }
+                    else
+                    {
+                        return "The PC wins!";
+                    }
+                case "paper":
+                    if (randomPcChoice == "rock")
+                    {
+                        return "The user wins!";
+                    }
+                    else
+                    {
+                        return "The PC wins!";
+                    }
+                case "scissors":
+                    if (randomPcChoice == "paper")
+                    {
+                        return "The user wins!";
+                    }
+                    else
+                    {
+                        return "The PC wins!";
+                    }
+                default: return "Some error happened";
+            }
+        }
 
 
         /* ----------------------------- StartGame Game ----------------------------- */
@@ -36,17 +83,16 @@ class Program
             {
                 if (isFirstTime)
                 {
-
-                    Console.WriteLine("Do you want to play rock, paper, scissors? y/n");
                     isFirstTime = false;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input! Try again:");
-                    Console.WriteLine("Do you want to play rock, paper, scissors?");
-                    Console.WriteLine("Enter y for `yes` or n for `no`.");
                     Console.WriteLine();
+                    Console.WriteLine("Invalid input! Try again:");
                 }
+                Console.WriteLine();
+                Console.WriteLine("Do you want to play rock, paper, scissors?");
+                Console.WriteLine("Enter y for `yes` or n for `no`.");
                 input = Console.ReadLine()?.ToLower();
             }
             while (input != "n" && input != "y");
