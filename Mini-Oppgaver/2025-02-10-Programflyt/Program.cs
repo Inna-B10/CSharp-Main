098579d70bd35ddb80637485deec777fb3f6aa23
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-11 03:06:10 +0100

    get user's choice, randomize PC choice

--- Mini-Oppgaver/2025-02-10-Programflyt/Program.cs
+++ Mini-Oppgaver/2025-02-10-Programflyt/Program.cs
@@ -4,20 +4,92 @@ class Program
 {
     static void Main(string[] args)
     {
-        string startGame()
+        string[] options = { "rock", "paper", "scissors" };
+        // Console.WriteLine(options[0]);
+
+
+
+
+
+        string? start = startGame();
+        if (start == "n") { return; }
+        Console.WriteLine("-----------------Game started-----------------");
+
+        string randomPcChoice = pcChoice(options);
+        Console.WriteLine("PC choice is: ***********");
+        Console.WriteLine("What is your choice?");
+        string? userChoice = getUserChoice();
+
+        Console.WriteLine();
+        Console.WriteLine($"User's choice is: {userChoice}");
+        Console.WriteLine($"PC's choice is: {randomPcChoice}");
+
+
+
+
+        /* ----------------------------- StartGame Game ----------------------------- */
+        string? startGame()
         {
-            Console.WriteLine("Do you want to play rock, paper, scissors? y/n");
-            return Console.ReadLine().ToLower();
+            string? input;
+            bool isFirstTime = true;
+            do
+            {
+                if (isFirstTime)
+                {
+
+                    Console.WriteLine("Do you want to play rock, paper, scissors? y/n");
+                    isFirstTime = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Try again:");
+                    Console.WriteLine("Do you want to play rock, paper, scissors?");
+                    Console.WriteLine("Enter y for `yes` or n for `no`.");
+                    Console.WriteLine();
+                }
+                input = Console.ReadLine()?.ToLower();
+            }
+            while (input != "n" && input != "y");
+            return input;
+        }
+        /* -------------------------------- PC Choice ------------------------------- */
+        string pcChoice(string[] options)
+        {
+            Random random = new Random();
+            //generate random index
+            int index = random.Next(options.Length);
+            return options[index];
         }
 
-        string userInput = startGame();
-        if (userInput == "n") { return; }
-        else if (userInput != "n" && userInput != "y")
+        /* ------------------------------ User's Choice ----------------------------- */
+        string getUserChoice()
         {
-            Console.WriteLine("Invalid input! Choose y for `yes` or n for `no`.");
-            startGame();
+            string? input;
+            bool isFirstTime = true;
+            do
+            {
+                if (isFirstTime) { isFirstTime = false; }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input! Try again:");
+                }
+                Console.WriteLine("Enter r for `rock`, p for `paper` or s for `scissors`");
+                input = Console.ReadLine()?.ToLower();
+            }
+            while (input != "r" && input != "p" && input != "s");
+
+            string choice = input switch
+            {
+                "r" => "rock",
+                "p" => "paper",
+                "s" => "scissors",
+            };
+            return choice;
         }
 
-        Console.WriteLine("yes");
+
+
+
     }
 }
