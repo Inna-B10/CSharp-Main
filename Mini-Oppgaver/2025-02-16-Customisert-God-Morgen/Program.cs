6911747733e7a45feca798d40a968b329e6906dc
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-16 16:49:00 +0100

    Customisert_God_Morgen: get userName

--- Mini-Oppgaver/2025-02-16-Customisert-God-Morgen/Program.cs
+++ Mini-Oppgaver/2025-02-16-Customisert-God-Morgen/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please, enter your name");
+        string userName = GetValidInput("name");
+        Console.WriteLine($"Welcome {userName}!");
+    }
+
+    public static string GetValidInput(string type = "name")
+    {
+        string? input = Console.ReadLine()?.Trim();
+
+        while (string.IsNullOrEmpty(input))
+        {
+            switch (type)
+            {
+                case "name":
+                    Console.WriteLine("Invalid input! Please, enter your name");
+                    input = Console.ReadLine()?.Trim();
+                    break;
+                case "genre":
+                    Console.WriteLine("Invalid input! Please select one of the genres:");
+                    Console.WriteLine("Rock, Pop, Classic or Electronic");
+                    input = Console.ReadLine()?.Trim();
+                    break;
+            }
+        }
+        return input;
     }
 }
