d2ed3dddb69513ba2bb77b61dad9246f583ff4e9
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-16 17:11:05 +0100

    Customisert_God_Morgen: get genre selected by user

--- Mini-Oppgaver/2025-02-16-Customisert-God-Morgen/Program.cs
+++ Mini-Oppgaver/2025-02-16-Customisert-God-Morgen/Program.cs
@@ -7,8 +7,38 @@ class Program
         Console.WriteLine("Please, enter your name");
         string userName = GetValidInput("name");
         Console.WriteLine($"Welcome {userName}!");
+        DateTime date = DateTime.Now;
+        GetMessage(date.Hour);
+        Console.WriteLine("What genre do you prefer?");
+        Console.WriteLine("Rock, pop, classic, electronic");
+        string genre = GetValidInput("genre");
+        Console.WriteLine(genre);
     }
 
+    public static void GetMessage(int time)
+    {
+
+        switch (time)
+        {
+            case > 17:
+                Console.WriteLine("Winding down?");
+                Console.WriteLine("Relaxing music helps create an atmosphere of comfort and harmony in the evening.");
+                break;
+            case > 12:
+                Console.WriteLine("Boosting your day?");
+                Console.WriteLine("Well-chosen melodies enhance productivity throughout the workday.");
+                break;
+            case > 5:
+                Console.WriteLine("Starting the day with energy?");
+                Console.WriteLine("Energetic music is the perfect way to start the day with vigor and motivation.");
+                break;
+            case > 0:
+                Console.WriteLine("Late-night tunes?");
+                Console.WriteLine("Soft, calming tunes prepare your mind for a good night's sleep.");
+                break;
+        }
+
+    }
     public static string GetValidInput(string type = "name")
     {
         string? input = Console.ReadLine()?.Trim();
@@ -27,6 +57,7 @@ class Program
                     input = Console.ReadLine()?.Trim();
                     break;
             }
+
         }
         return input;
     }
