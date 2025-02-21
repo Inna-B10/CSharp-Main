d38dee0e0b5ba75830e6159f81cb2427bd7bb907
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-02-21 14:54:51 +0100

    Simple_Calculator: trying validate input from user

--- Mini-Oppgaver/2025-02-21-Simple-Calculator/Program.cs
+++ Mini-Oppgaver/2025-02-21-Simple-Calculator/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace _2025_02_21_Simple_Calculator;
+﻿using System.ComponentModel;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _2025_02_21_Simple_Calculator;
 
 class Program
 {
+    public static char[] validOperand = ['+', '-', '/', '*'];
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? input;
+        bool firstRound = true;
+        do
+        {
+            if (firstRound) firstRound = false; else Console.WriteLine("Invalid input!");
+            Console.WriteLine("Please enter a mathematical expression");
+            input = Console.ReadLine()?.Trim();
+        }
+        while (!IsValidInput(input));
+
+
+        input = input?.Replace(" ", "");
+        Console.WriteLine(input);
+
+
+
+
+        // double firstNumber = 0;
+        //         try
+        //         {
+        // 
+        //             double result = Calculator<double>.Calc(4, 0, '%');
+        //             Console.WriteLine(result);
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             Console.WriteLine(ex.Message);
+        //             throw;
+        //         }
     }
+
+    class Calculator<T>
+    where T : INumber<T>
+    {
+        public static T Calc(T a, T b, char operand)
+        {
+            return operand switch
+            {
+                '+' => a + b,
+                '-' => a - b,
+                '*' => a * b,
+                '/' => b != T.Zero ? a / b : throw new Exception("Cannot divide by 0"),
+                _ => throw new Exception("Invalid operand.")
+
+            };
+        }
+    }
+
+    public static bool IsValidInput(string? expression)
+    {
+        if (string.IsNullOrEmpty(expression)) return false;
+        if (Regex.IsMatch(expression, "[+\\-*/]{2,}")) return false;
+        if (Regex.IsMatch(expression, "^[+\\-*/]")) return false;
+        if (Regex.IsMatch(expression, "[+\\-*/]$")) return false;
+
+        return true;
+    }
+
 }
