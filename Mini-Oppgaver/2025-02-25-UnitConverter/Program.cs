6506ff8fa11685bdaf910d47632268ad12e4f4ad
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-26 00:08:26 +0100

    UnitConverter: Clean up code

--- Mini-Oppgaver/2025-02-25-UnitConverter/Program.cs
+++ Mini-Oppgaver/2025-02-25-UnitConverter/Program.cs
@@ -5,6 +5,15 @@ namespace _2025_02_25_UnitConverter;
 
 partial class Program
 {
+  /* ---------------------------------- Regex --------------------------------- */
+  [GeneratedRegex(@"^(\d+([.,]\d+)?)[mfklMFKL]=[mfklMFKL]$")]
+  private static partial Regex LengthRegex();
+
+  [GeneratedRegex(@"^(\d+([.,]\d+)?)([A-Za-z]+)$")]
+  private static partial Regex isAmountExistRegex();
+  [GeneratedRegex(@"^(\d+(\.\d+)?)([A-Za-z]+)=([A-Za-z]+)$")]
+  private static partial Regex ExpressionRegex();
+  /* ---------------------------------- Enums --------------------------------- */
   public enum LengthUnits
   {
     meters, kilometers, miles, feet
@@ -13,6 +22,7 @@ partial class Program
   {
     USD, EUR, NOK, ILS, GBP
   }
+  /* ------------------------------ Dictionaries ------------------------------ */
   public static Dictionary<LengthUnits, double> length = new()
         {
             { LengthUnits.meters, 1 },
@@ -27,73 +37,35 @@ partial class Program
             { CurrencyUnits.NOK, 11.10 },
             { CurrencyUnits.ILS, 3.59 },
             { CurrencyUnits.GBP, 0.79 },
-
         };
-  public static string FormatNumber(double value)
-  {
-    var culture = new CultureInfo("no-NO");
-
-    // If the fractional part == 0, display without the decimal part, otherwise with two signs
-    return value % 1 == 0
-        ? value.ToString("N0", culture)  // "N0" -> format with thousands separators, no decimal part
-        : value.ToString("G", culture); // "N2" -> 2 decimal places (can be changed to "G" for auto)
-  }
-
+  /* ---------------------------------- Main ---------------------------------- */
   static void Main()
   {
-
     Console.WriteLine("Hello, user!");
-    UserChoice();
+    string choice = GetUserChoice();
 
-    //         try
-    //         {
-    //             var lengthConverter = new UnitConverter<LengthUnits>(length);
-    //             Console.WriteLine("2 miles in meters: " + FormatNumber(lengthConverter.ConvertToBase(LengthUnits.miles, 2)));
-    //             Console.WriteLine("100 meters in feet: " + FormatNumber(lengthConverter.ConvertFromBase(LengthUnits.feet, 100)));
-    //             Console.WriteLine("5 kilometers in miles: " + FormatNumber(lengthConverter.ConvertDirect(LengthUnits.kilometers, LengthUnits.miles, 5)));
-    // 
-    //             var currencyConverter = new UnitConverter<CurrencyUnits>(currency);
-    //             double result = currencyConverter.ConvertCurrencies(CurrencyUnits.EUR, CurrencyUnits.NOK, 15);
-    //             string convertedValue = FormatNumber(result);
-    //             Console.WriteLine("15 Euro in Norwegian Kroner : " + convertedValue);
-    //         }
-    //         catch (ArgumentNullException arNull)
-    //         {
-    //             Console.WriteLine("Error: " + arNull.Message);
-    //         }
-    //         catch (Exception ex)
-    //         {
-    //             Console.WriteLine("Error: " + ex.Message);
-    //         }
-
-  }
-  public static string ConvertUnitName(string part)
-  {
-    return part switch
+    if (choice == "error")
     {
-      "m" => "meters",
-      "k" => "kilometers",
-      "l" => "miles",
-      "f" => "feet",
-      _ => ""
-    };
-  }
-  public static string[] ParseInput(string expression, string type)
-  {
-    expression = expression.Replace(",", ".");
-    // var match = Regex.Match(expression, @"^(\d+(\.\d+)?)([A-Za-z]+)=([A-Za-z]+)$");
-    var match = ExpressionRegex().Match(expression);
-    string amount = match.Groups[1].Value;
-    string from = type == "length" ? ConvertUnitName(match.Groups[3].Value) : match.Groups[3].Value;
-    string to = type == "length" ? ConvertUnitName(match.Groups[4].Value) : match.Groups[4].Value;
+      Console.WriteLine("Some error occurred. The program is terminating.");
+      return;
+    }
+    if (choice == "length") LengthConverter();
 
-    return [amount, from, to];
+    if (choice == "currency") CurrencyConverter();
   }
+  /* ------------------------------ GetUserChoice ----------------------------- */
+  public static string GetUserChoice()
+  {
+    Console.WriteLine("What do you want to convert? [1] length, [2] currency");
+    return GetValidInput("choice");
+  }
+  /* ----------------------------- LengthConverter ---------------------------- */
   public static void LengthConverter()
   {
     Console.WriteLine("4 values are available for conversion");
     Console.WriteLine("[m] meters, [k] kilometers, [l] miles, [f] feet");
     Console.WriteLine("valid format: from=to, f.ex to convert 5 kilometers to meters write 5k=m");
+    // Converter(choice);
 
     string userInput = GetValidInput("length");
     string[] expressionParts = ParseInput(userInput, "length");
@@ -108,10 +80,6 @@ partial class Program
         string result = FormatNumber(lengthConverter.ConvertDirect(fromUnit, toUnit, amount));
         Console.WriteLine($"{FormatNumber(amount)} {fromUnit} in {toUnit}: {result}");
       }
-      catch (ArgumentNullException arNull)
-      {
-        Console.WriteLine("Error: " + arNull.Message);
-      }
       catch (Exception ex)
       {
         Console.WriteLine("Error: " + ex.Message);
@@ -123,11 +91,12 @@ partial class Program
       return;
     }
   }
-
+  /* ---------------------------- CurrencyConverter --------------------------- */
   public static void CurrencyConverter()
   {
     Console.WriteLine("Currencies available for conversion: USD, EUR, NOK, ILS, GBP");
     Console.WriteLine("Valid format: from=to, f.ex to convert 10 eur to nok write 10eur=nok");
+    // Converter(choice);
     string userInput = GetValidInput("currency");
     Console.WriteLine(userInput);
     string[] expressionParts = ParseInput(userInput, "currency");
@@ -142,34 +111,13 @@ partial class Program
         string result = FormatNumber(currencyConverter.ConvertCurrencies(fromUnit, toUnit, amount));
         Console.WriteLine($"{FormatNumber(amount)} {fromUnit} in {toUnit}: {result}");
       }
-      catch (ArgumentNullException arNull)
-      {
-        Console.WriteLine("Error: " + arNull.Message);
-      }
       catch (Exception ex)
       {
         Console.WriteLine("Error: " + ex.Message);
       }
     }
   }
-
-  public static void UserChoice()
-  {
-    Console.WriteLine("What do you want to convert? [1] length, [2] currency");
-    string choice = GetValidInput("choice");
-
-    if (choice == "error")
-    {
-      Console.WriteLine("Some error occurred. The program is terminating.");
-      return;
-    }
-    else if (choice == "length")
-    {
-      LengthConverter();
-    }
-    else
-      CurrencyConverter();
-  }
+  /* ------------------------------ GetValidInput ----------------------------- */
   public static string GetValidInput(string type)
   {
     string? input;
@@ -195,36 +143,7 @@ partial class Program
     }
     return input;
   }
-  public static bool IsCurrencyInputValid(string? input)
-  {
-    if (string.IsNullOrWhiteSpace(input)) return false;
-
-    string[] parts = input.Split("=");
-    if (parts.Length != 2) return false;
-
-    return IsValidFormat(parts[0]) && Enum.TryParse(parts[1], true, out CurrencyUnits _);
-  }
-  private static bool IsValidFormat(string part)
-  {
-    // var match = Regex.Match(part, @"^(\d+(\.\d+)?)([A-Za-z]+)$");
-    var match = isAmountExistRegex().Match(part);
-
-    if (!match.Success) return false;
-
-    string currencyCode = match.Groups[3].Value;
-    return Enum.TryParse(currencyCode, true, out CurrencyUnits _);
-  }
-  public static bool IsValidInput(string? input, string type)
-  {
-    return type switch
-    {
-      "choice" => !string.IsNullOrWhiteSpace(input) && "12".Contains(input),
-      // "length" => !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, @"^(\d+([.,]\d+)?)[mfklMFKL]=[mfklMFKL]$"),
-      "length" => !string.IsNullOrWhiteSpace(input) && LengthRegex().IsMatch(input),
-      "currency" => IsCurrencyInputValid(input),
-      _ => false
-    };
-  }
+  /* ----------------------------- GetErrorMessage ---------------------------- */
   public static string GetErrorMessage(string type)
   {
     return type switch
@@ -235,21 +154,80 @@ partial class Program
       _ => "Unknown error"
     };
   }
+  /* ------------------------------ IsValidInput ------------------------------ */
+  public static bool IsValidInput(string? input, string type)
+  {
+    return type switch
+    {
+      "choice" => !string.IsNullOrWhiteSpace(input) && "12".Contains(input),
+      //NB "length" => !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, @"^(\d+([.,]\d+)?)[mfklMFKL]=[mfklMFKL]$"),
+      "length" => !string.IsNullOrWhiteSpace(input) && LengthRegex().IsMatch(input),
+      "currency" => IsCurrencyInputValid(input),
+      _ => false
+    };
+  }
+  /* -------------------------- IsCurrencyInputValid -------------------------- */
+  public static bool IsCurrencyInputValid(string? input)
+  {
+    if (string.IsNullOrWhiteSpace(input)) return false;
 
-  [GeneratedRegex(@"^(\d+([.,]\d+)?)[mfklMFKL]=[mfklMFKL]$")]
-  private static partial Regex LengthRegex();
+    string[] parts = input.Split("=");
+    if (parts.Length != 2) return false;
 
-  [GeneratedRegex(@"^(\d+([.,]\d+)?)([A-Za-z]+)$")]
-  private static partial Regex isAmountExistRegex();
-  [GeneratedRegex(@"^(\d+(\.\d+)?)([A-Za-z]+)=([A-Za-z]+)$")]
-  private static partial Regex ExpressionRegex();
+    return IsValidFormat(parts[0]) && Enum.TryParse(parts[1], true, out CurrencyUnits _);
+  }
+  /* ------------------------------ IsValidFormat ----------------------------- */
+  private static bool IsValidFormat(string part)
+  {
+    //NB var match = Regex.Match(part, @"^(\d+(\.\d+)?)([A-Za-z]+)$");
+    var match = isAmountExistRegex().Match(part);
+
+    if (!match.Success) return false;
+
+    string currencyCode = match.Groups[3].Value;
+    return Enum.TryParse(currencyCode, true, out CurrencyUnits _);
+  }
+  /* ------------------------------- ParseInput ------------------------------- */
+  public static string[] ParseInput(string expression, string type)
+  {
+    expression = expression.Replace(",", ".");
+    //NB var match = Regex.Match(expression, @"^(\d+(\.\d+)?)([A-Za-z]+)=([A-Za-z]+)$");
+    var match = ExpressionRegex().Match(expression);
+    string amount = match.Groups[1].Value;
+    string from = type == "length" ? ConvertUnitName(match.Groups[3].Value) : match.Groups[3].Value;
+    string to = type == "length" ? ConvertUnitName(match.Groups[4].Value) : match.Groups[4].Value;
+
+    return [amount, from, to];
+  }
+  /* ----------------------------- ConvertUnitName ---------------------------- */
+  public static string ConvertUnitName(string part)
+  {
+    return part switch
+    {
+      "m" => "meters",
+      "k" => "kilometers",
+      "l" => "miles",
+      "f" => "feet",
+      _ => ""
+    };
+  }
+  /* ------------------------------ FormatNumber ------------------------------ */
+  public static string FormatNumber(double value)
+  {
+    var culture = new CultureInfo("no-NO");
+
+    // If the fractional part == 0, display without the decimal part
+    return value % 1 == 0
+        ? value.ToString("N0", culture)  //NB "N0" -> format with thousands separators, no decimal part
+        : value.ToString("G", culture);  //NB "N2" or"N3" -> 2 or 3 decimal places ("G" for auto)
+  }
 }
-
-
+/* --------------------------- Class UnitConverter -------------------------- */
 public class UnitConverter<T>(Dictionary<T, double> rates)
 where T : Enum
 {
-  private readonly Dictionary<T, double> conversionRates = rates ?? throw new ArgumentNullException(nameof(rates));
+  private readonly Dictionary<T, double> conversionRates = rates ?? throw new Exception("Error: Value cannot be null. (Parameter 'rates')");
+  /*  ConvertToBase ----------------------------- */
   public double ConvertToBase(T from, double amount)
   {
     if (!conversionRates.TryGetValue(from, out double value))
@@ -257,6 +235,7 @@ where T : Enum
 
     return amount * value;
   }
+  /*  ConvertFromBase ---------------------------- */
   public double ConvertFromBase(T to, double amount)
   {
     if (!conversionRates.TryGetValue(to, out double value))
@@ -264,6 +243,7 @@ where T : Enum
 
     return amount / value;
   }
+  /*  ConvertDirect ----------------------------- */
   public double ConvertDirect(T from, T to, double amount)
   {
     if (!conversionRates.TryGetValue(from, out _) || (!conversionRates.TryGetValue(to, out _)))
@@ -272,6 +252,7 @@ where T : Enum
     double baseValue = ConvertToBase(from, amount);
     return ConvertFromBase(to, baseValue);
   }
+  /*  ConvertCurrencies --------------------------- */
   public double ConvertCurrencies(T from, T to, double amount)
   {
     if (!conversionRates.TryGetValue(from, out _) || (!conversionRates.TryGetValue(to, out _)))
