8dbf5f2fdf99dc0f0b476cb65ce5ce9e31affb82
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-02-27 15:14:47 +0100

    Lego_database: initial files

---
+++ Mini-Oppgaver/2025-02-27-Lego-database/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _2025_02_27_Lego_database;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Mini-Oppgaver/2025-02-27-Lego-database/Readme.md
@@ -0,0 +1,1 @@
+[Excalidraw file](https://excalidraw.com/#json=L3m_hFWsiLQ8NAbbY-RRk,zEjI9aHyCOIhSJBJlLM3xw)
