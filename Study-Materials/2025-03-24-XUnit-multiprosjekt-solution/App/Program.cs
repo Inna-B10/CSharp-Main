41fb4b5d73c3e63a22ebf517fae8761912be1af6
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-25 09:44:58 +0100

    added new material: XUnit-multiprosjekt-solution

---
+++ Study-Materials/2025-03-24-XUnit-multiprosjekt-solution/App/Program.cs
@@ -0,0 +1,5 @@
+﻿/* Siden vi også linket vårt Core library til 
+vår console app, kan vi også bruke klassene her også.*/
+using Core.Classes;
+
+var bear = new Bear("Ole", "Black", 20, Feed.Omnivore);
