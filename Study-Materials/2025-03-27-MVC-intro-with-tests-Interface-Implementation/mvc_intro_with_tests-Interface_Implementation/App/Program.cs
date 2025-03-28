13922a4cb3330d94d5e71ab0207973226a7bc8e8
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-28 10:45:34 +0100

    added new material: MVC-intro-with-tests

---
+++ Study-Materials/2025-03-27-MVC-intro-with-tests-Interface-Implementation/mvc_intro_with_tests-Interface_Implementation/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Core.Classes.Controller;
+using Core.Classes.Models;
+using Core.Classes.Views;
+
+namespace App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var controller = new TaskController();
+        controller.Run();
+    }
+}
