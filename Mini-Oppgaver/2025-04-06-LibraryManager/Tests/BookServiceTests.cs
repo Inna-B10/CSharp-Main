8f559d513b9e44519b6accc88fc4bfa00874947c
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-04-11 19:32:15 +0200

    LibraryManager: updated BookTests, added more tests in BookService

--- Mini-Oppgaver/2025-04-06-LibraryManager/Tests/BookServiceTests.cs
+++ Mini-Oppgaver/2025-04-06-LibraryManager/Tests/BookServiceTests.cs
@@ -14,8 +14,9 @@ public class BookServiceTests
     _service.AddBook("To Kill a Mockingbird", "Harper Lee");
   }
 
-  [Fact]
-  public void AddBook_Should_Add_Book_And_return_true()
+  //* ------------------------------ Test AddBook ------------------------------ */
+  [Fact(DisplayName = "AddBook: Should return TRUE if book added")]
+  public void AddBook_Test()
   {
     //Arrange
     var title = "Pride and Prejudice";
@@ -26,5 +27,175 @@ public class BookServiceTests
 
     //Assert
     Assert.True(result);
+  }
+
+  //* --------------------- Test GetBookById With Valid Id --------------------- */
+  [Fact(DisplayName = "GetBookById: Should return correct book details")]
+  public void GetBookById_Test_With_ValidId()
+  {
+    //Arrange
+    int id = 1;
+
+    //Act
+    var book = _service.GetBookById(id);
+
+    //Assert
+    Assert.NotNull(book);
+    Assert.Equal(id, book.Id);
+  }
+
+  //* -------------------- Test GetBookById With Invalid Id -------------------- */
+  [Fact(DisplayName = "GetBookById: Should return Null if book not found")]
+  public void GetBookById_Test_With_InvalidId()
+  {
+    //Arrange
+    int id = 100;
+
+    //Act
+    var book = _service.GetBookById(id);
+
+    //Assert
+    Assert.Null(book);
+  }
+
+  /* -------------------------- Test GetBorrowedBooks ------------------------- */
+  //   [Fact(DisplayName = "Should return list only of borrowed books")]
+  //   public void GetBorrowedBooks_Test()
+  //   {
+  // //Arrange
+  // int id = 1;
+  // _service.BorrowBook(id);
+  // 
+  // //Act
+  // var borrowedBooks= _service.GetBorrowedBooks();
+  // 
+  // //Assert
+  // Assert.All(borrowedBooks, book=>Assert.True(book.IsBorrowed));
+  //   }
+
+  //* ---------------------- Test BorrowBook With Valid Id --------------------- */
+  [Fact(DisplayName = "BorrowBook: Should return new dueDate")]
+  public void BorrowBook_Test_With_ValidId()
+  {
+    //Arrange
+    int id = 1;
+    var expectedDueDate = DateTime.Today.AddDays(14);
+
+    //Act
+    var actualDueDate = _service.BorrowBook(id);
+
+    //Assert
+    Assert.Equal(expectedDueDate, actualDueDate);
+  }
+
+  //* --------------------- Test BorrowBook With Invalid Id -------------------- */
+  [Fact(DisplayName = "BorrowBook: Should return NULL if book not found")]
+  public void BorrowBook_Test_With_InvalidId()
+  {
+    //Arrange
+    int invalidId = 99;
+
+    //Act
+    var result = _service.BorrowBook(invalidId);
+
+    //Assert
+    Assert.Null(result);
+  }
+
+  //* ------------------ Test BorrowBook When Already Borrowed ----------------- */
+  [Fact(DisplayName = "BorrowBook: Should return NULL if book already borrowed")]
+  public void BorrowBook_Test_When_Already_Borrowed()
+  {
+    //Arrange
+    int id = 2;
+    var book = _service.GetBookById(id);
+    book!.IsBorrowed = true;
+
+    //Act
+    var result = _service.BorrowBook(id);
+
+    //Assert
+    Assert.Null(result);
+  }
+
+  //* --------------------------- Test MarkAsBorrowed -------------------------- */
+  [Fact(DisplayName = "MarkAsBorrowed: Should set IsBorrowed TRUE, set and return new dueDate ")]
+  public void MarkAsBorrowed_Test()
+  {
+    // Arrange
+    int id = 2;
+    var book = _service.GetBookById(id);
+    book!.IsBorrowed = false;
+    var dueDate = DateTime.Today.AddDays(14);
+
+    // Act
+    _service.MarkAsBorrowed(book);
+
+    // Assert
+    Assert.True(book.IsBorrowed);
+    Assert.Equal(dueDate, book.DueDate);
+  }
+
+  //* ---------------------- Test ReturnBook With Valid Id --------------------- */
+  [Fact(DisplayName = "ReturnBook: Should return TRUE")]
+  public void ReturnBook_Test_With_ValidId()
+  {
+    //Arrange
+    int id = 2;
+    var book = _service.GetBookById(id);
+    book!.IsBorrowed = true;
+
+    //Act
+    var result = _service.ReturnBook(id);
+
+    //Assert
+    Assert.True(result);
+  }
+
+  //* --------------------- Test ReturnBook With Invalid Id -------------------- */
+  [Fact(DisplayName = "ReturnBook: Should return FALSE if book not found")]
+  public void ReturnBook_Test_With_InvalidId()
+  {
+    //Arrange
+    int id = 99;
+
+    //Act
+    var result = _service.ReturnBook(id);
+
+    //Assert
+    Assert.False(result);
+  }
+
+  //* ----------------- Test ReturnBook When Book Not Borrowed ----------------- */
+  [Fact(DisplayName = "ReturnBook: Should return FALSE if book not borrowed")]
+  public void ReturnBook_Test_When_Book_Avlabile()
+  {
+    //Arrange
+    int id = 2;
+    var book = _service.GetBookById(id);
+    book!.IsBorrowed = false;
+
+    //Act
+    var result = _service.ReturnBook(id);
+
+    //Assert
+    Assert.False(result);
+  }
+
+  //* --------------------------- Test MarkAsReturned -------------------------- */
+  [Fact(DisplayName = "Should set IsBorrowed to FALSE and dueDate to NULL")]
+  public void MarkAsReturned_Test()
+  {
+    //Arrange
+    int id = 3;
+    var book = _service.GetBookById(id);
+    book!.IsBorrowed = true;
+
+    //Act
+    _service.MarkAsReturned(book);
+
+    //Assert
+    Assert.False(book.IsBorrowed);
+    Assert.Null(book.DueDate);
   }
 }
--- Mini-Oppgaver/2025-04-06-LibraryManager/Tests/BookTests.cs
+++ Mini-Oppgaver/2025-04-06-LibraryManager/Tests/BookTests.cs
@@ -1,13 +1,11 @@
-using Core.Classes;
 using Core.Classes.Models;
-using Core.Interfaces;
-
 namespace Tests;
 
 public class BookTests
 {
-    [Fact]
-    public void Book_Constructor_Should_Set_Properties_Correctly()
+    //* ----------------------------- Set_Properties ----------------------------- */
+    [Fact(DisplayName = "Should set properties correctly")]
+    public void Book_Constructor_Test()
     {
         // Arrange
         int id = 1;
@@ -21,40 +19,6 @@ public class BookTests
         Assert.Equal(id, book.Id);
         Assert.Equal(title, book.Title);
         Assert.Equal(author, book.Author);
-        Assert.False(book.IsBorrowed);
-        Assert.Null(book.DueDate);
-    }
-
-    [Fact(DisplayName = "Должен установить флаг IsBorrowed в true")]
-    public void MarkAsBorrowed_Should_Set_IsBorrowed_True_And_Set_DueDate()
-    {
-        // Arrange
-        var book = new Book(2, "Brave New World", "Aldous Huxley");
-        Assert.False(book.IsBorrowed);
-        // var dueDate = new DateTime(2025, 4, 15);
-        var dueDate = DateTime.Now.AddDays(14);
-
-        // Act
-        book.MarkAsBorrowed(dueDate);
-
-        // Assert
-        Assert.True(book.IsBorrowed);
-        Assert.Equal(dueDate, book.DueDate);
-
-    }
-    [Fact]
-    public void MarkAsReturned_Should_Set_IsBorrowed_False_And_Clear_DueDate()
-    {
-        //Arrange
-        var book = new Book(3, "Fahrenheit 451", "Ray Bradbury");
-        var dueDate = DateTime.Now.AddDays(14);
-        Assert.False(book.IsBorrowed);
-        book.MarkAsBorrowed(dueDate);
-
-        //Act
-        book.MarkAsReturned();
-
-        //Assert
         Assert.False(book.IsBorrowed);
         Assert.Null(book.DueDate);
     }
