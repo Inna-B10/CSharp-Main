f9abbd867e8d50f81b476749d13032c74ed64aa6
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-04-22 13:44:09 +0200

    added new material: TDD-v2-intro

---
+++ Study-Materials/2025-04-22-TDD-v2-intro/Calc.Tests/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿namespace Calc.Tests;
+
+public class Addition
+{
+    [Theory]
+    [InlineData(1, 1, 2)]
+    [InlineData(0, 1, 1)]
+    [InlineData(-10, -10, -20)]
+    public void SumsTwoNumber(float a, float b, float expected)
+    {
+        // Arrange
+        var calc = new Math.Calc();
+
+        // Act
+        var result = calc.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void HandlesOverflow()
+    {
+        // Arrange
+        var calc = new Math.Calc();
+        float a = float.MaxValue;
+        float b = float.MaxValue;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => calc.Add(a, b));
+    }
+
+    [Fact]
+    public void HandlesUnderflow()
+    {
+        // Arrange
+        var calc = new Math.Calc();
+        float a = float.MinValue;
+        float b = float.MinValue;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => calc.Add(a, b));
+    }
+}
+
+public class Division
+{
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(2, 1, 2)]
+    public void DividesTwoNumbers(float a, float b, float expected)
+    {
+        // Arrange
+        var calc = new Math.Calc();
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void DivisionByZero()
+    {
+        // Arrange
+        var calc = new Math.Calc();
+        float a = 1;
+        float b = 0;
+        float expected = float.PositiveInfinity;
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Study-Materials/2025-04-22-TDD-v2-intro/Math/Calc.cs
@@ -0,0 +1,21 @@
+﻿namespace Math;
+
+public class Calc
+{
+  public float Add(float a, float b)
+  {
+    var result = a + b;
+
+    if (float.IsInfinity(result))
+    {
+      throw new ArgumentException("Numbers to big!");
+    }
+
+    return a + b;
+  }
+
+  public float Divide(float a, float b)
+  {
+    return a / b;
+  }
+}
