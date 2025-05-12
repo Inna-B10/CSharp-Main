1e5e2634526b010c937685a6017b9d81b28b4ede
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-05-12 15:22:58 +0200

    added new material: SOA-composition-main

---
+++ Study-Materials/2025-05-08-SOA-composition-main/infrastructure/modules/gitops_lite/main.tf
@@ -0,0 +1,20 @@
+resource "hcloud_server" "server" {
+  name        = "gitops-server"
+  image       = "debian-12"
+  server_type = var.server_type
+  location    = var.location
+
+  user_data = templatefile("${path.module}/cloud-init.tftpl", {
+    ssh_public_key   = var.ssh_public_key
+    git_source       = var.git_source
+    target_branch    = var.target_branch
+    compose_path     = var.compose_path
+    refresh_interval = var.update_interval
+    hostname         = "gitops-server"
+  })
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = false
+  }
+}
